730fee48b492e9eb85f211d2d4a11f141de1fdd4
Jorgenor56 <132491623+Jorgenor56@users.noreply.github.com>
2023-12-05 11:54:46 +0100

    UsaAccesoDOM.java

--- EjercicioLecturaEscritura/src/UsaAccesoDOM.java
+++ EjercicioLecturaEscritura/src/UsaAccesoDOM.java
@@ -3,24 +3,23 @@ import java.io.File;
 public class UsaAccesoDOM {
     public static void main(String[] args) {
         AccesoDOM accesoDOM = new AccesoDOM();
-        File archivoXML = new File("Libros.xml"); // Reemplaza con la ubicación de tu archivo XML
+        //Reemplaza con la ubicación de tu archivo XML
+        File archivoXML = new File("Libros.xml");
 
-        // Abrir el archivo XML
-        int resultadoApertura = accesoDOM.abriXMLaDOM(archivoXML);
+        //Abrir el archivo XML
+        int resultadoApertura = accesoDOM.abrirXMLaDOM(archivoXML);
         if (resultadoApertura == 0) {
-            // Mostrar el contenido del archivo XML
+            //Mostrar el contenido del archivo XML
             accesoDOM.recorreDOMyMuestra();
 
-            // Insertar un libro en el DOM
+            //Insertar un libro en el DOM
             accesoDOM.insertarLibroEnDOM("Yerma", "Lorca", "1935");
 
-            // Borrar un nodo por título
+            //Borrar un nodo por título
             accesoDOM.deleteNode("Don Quijote");
 
-            // Guardar el DOM modificado en un nuevo archivo
+            //Guardar el DOM modificado en un nuevo archivo
             accesoDOM.guardarDOMcomoArchivo("LibrosDeDOM.xml");
         }
     }
 }
-
-
